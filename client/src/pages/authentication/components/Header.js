9c7109070720bd2f8dc71607223db2b95a41ca53
Nabil Yahyaoui <yahyaouinabil1993@gmail.com>
2024-02-12 10:22:01 +0100

    responsive design for login and register page

--- client/src/pages/authentication/components/Header.js
+++ client/src/pages/authentication/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="flex justify-center items-center w-1/3 h-12 bg-white rounded-md border-2 border-green-200">
+    <div className="flex justify-center items-center lg:w-1/3 md:w-1/2 w-11/12 h-12 bg-white rounded-md border-2 border-green-200">
       <p className="text-center text-xl uppercase font-bold text-green-400">
         IJA-NAHKIW
       </p>
